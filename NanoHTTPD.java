6e946e072ac9e2f61e00e4dfddd23ff6f35f3db8
elonen <elonen@iki.fi>
2010-12-31 00:38:58 +0000

    File upload support by ktogias. Version 1.2

--- NanoHTTPD.java
+++ NanoHTTPD.java
@@ -12,17 +12,22 @@ import java.net.Socket;
 import java.net.URLEncoder;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.Vector;
 import java.util.Hashtable;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.StringTokenizer;
 import java.util.TimeZone;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+
 /**
  * A simple, tiny, nicely embeddable HTTP 1.0 server in Java
  *
- * <p> NanoHTTPD version 1.14,
+ * <p> NanoHTTPD version 1.2,
  * Copyright &copy; 2001,2005-2010 Jarno Elonen (elonen@iki.fi, http://iki.fi/elonen/)
+ * and Copyright &copy; 2010 Konstantinos Togias (info@ktogias.gr, http://ktogias.gr)
  *
  * <p><b>Features + limitations: </b><ul>
  *
@@ -32,6 +37,7 @@ import java.util.TimeZone;
  *    <li> No fixed config files, logging, authorization etc. (Implement yourself if you need them.) </li>
  *    <li> Supports parameter parsing of GET and POST methods </li>
  *    <li> Supports both dynamic content and file serving </li>
+ *    <li> Supports file upload (since version 1.2, 2010) </li>
  *    <li> Never caches anything </li>
  *    <li> Doesn't limit bandwidth, request time or simultaneous connections </li>
  *    <li> Default code serves files and shows all HTTP parameters and headers</li>
@@ -73,7 +79,7 @@ public class NanoHTTPD
 	 * @parm header	Header entries, percent decoded
 	 * @return HTTP response, see class Response for details
 	 */
-	public Response serve( String uri, String method, Properties header, Properties parms )
+	public Response serve( String uri, String method, Properties header, Properties parms, Properties files )
 	{
 		System.out.println( method + " '" + uri + "' " );
 
@@ -90,6 +96,13 @@ public class NanoHTTPD
 			String value = (String)e.nextElement();
 			System.out.println( "  PRM: '" + value + "' = '" +
 								parms.getProperty( value ) + "'" );
+		}
+		e = files.propertyNames();
+		while ( e.hasMoreElements())
+		{
+			String value = (String)e.nextElement();
+			System.out.println( "  UPLOADED: '" + value + "' = '" +
+								files.getProperty( value ) + "'" );
 		}
 
 		return serveFile( uri, header, new File("."), true );
@@ -127,7 +140,14 @@ public class NanoHTTPD
 		{
 			this.status = status;
 			this.mimeType = mimeType;
-			this.data = new ByteArrayInputStream( txt.getBytes());
+			try
+			{
+				this.data = new ByteArrayInputStream( txt.getBytes("UTF-8"));
+			}
+			catch ( java.io.UnsupportedEncodingException uee )
+			{
+				uee.printStackTrace();
+			}
 		}
 
 		/**
@@ -178,12 +198,13 @@ public class NanoHTTPD
 	public static final String
 		MIME_PLAINTEXT = "text/plain",
 		MIME_HTML = "text/html",
-		MIME_DEFAULT_BINARY = "application/octet-stream";
+		MIME_DEFAULT_BINARY = "application/octet-stream",
+		MIME_XML = "text/xml";
 
 	// ==================================================
 	// Socket & server code
 	// ==================================================
-	
+
 	/**
 	 * Starts a HTTP server to given port.<p>
 	 * Throws an IOException if the socket is already in use
@@ -215,7 +236,7 @@ public class NanoHTTPD
 	public void stop()
 	{
 		try
-		{ 
+		{
 			myServerSocket.close();
 			myThread.join();
 		}
@@ -229,7 +250,7 @@ public class NanoHTTPD
 	 */
 	public static void main( String[] args )
 	{
-		System.out.println( "NanoHTTPD 1.14 (C) 2001,2005-2010 Jarno Elonen\n" +
+		System.out.println( "NanoHTTPD 1.2 (C) 2001,2005-2010 Jarno Elonen and (C) 2010 Konstantinos Togias\n" +
 							"(Command line options: [port] [--licence])\n" );
 
 		// Show licence if requested
@@ -289,76 +310,124 @@ public class NanoHTTPD
 			{
 				InputStream is = mySocket.getInputStream();
 				if ( is == null) return;
-				BufferedReader in = new BufferedReader( new InputStreamReader( is ));
 
-				// Read the request line
-				String inLine = in.readLine();
-				if (inLine == null) return;
-				StringTokenizer st = new StringTokenizer( inLine );
-				if ( !st.hasMoreTokens())
-					sendError( HTTP_BADREQUEST, "BAD REQUEST: Syntax error. Usage: GET /example/file.html" );
+				// Read the first 8192 bytes.
+				// The full header should fit in here.
+				// Apache's default header limit is 8KB.
+				int bufsize = 8192;
+				byte[] buf = new byte[bufsize];
+				int rlen = is.read(buf, 0, bufsize);
+				if (rlen <= 0)
+				return;
 
-				String method = st.nextToken();
-
-				if ( !st.hasMoreTokens())
-					sendError( HTTP_BADREQUEST, "BAD REQUEST: Missing URI. Usage: GET /example/file.html" );
-
-				String uri = st.nextToken();
-
-				// Decode parameters from the URI
+				// Create a BufferedReader for parsing the header.
+				ByteArrayInputStream hbis = new ByteArrayInputStream(buf, 0, rlen);
+				BufferedReader hin = new BufferedReader( new InputStreamReader( hbis ));
+				Properties pre = new Properties();
 				Properties parms = new Properties();
-				int qmi = uri.indexOf( '?' );
-				if ( qmi >= 0 )
-				{
-					decodeParms( uri.substring( qmi+1 ), parms );
-					uri = decodePercent( uri.substring( 0, qmi ));
-				}
-				else uri = decodePercent(uri);
-
-
-				// If there's another token, it's protocol version,
-				// followed by HTTP headers. Ignore version but parse headers.
-				// NOTE: this now forces header names uppercase since they are
-				// case insensitive and vary by client.
 				Properties header = new Properties();
-				if ( st.hasMoreTokens())
+				Properties files = new Properties();
+
+				// Decode the header into parms and header java properties
+				decodeHeader(hin, pre, parms, header);
+				String method = pre.getProperty("method");
+				String uri = pre.getProperty("uri");
+
+				long size = 0x7FFFFFFFFFFFFFFFl;
+				String contentLength = header.getProperty("content-length");
+				if (contentLength != null)
 				{
-					String line = in.readLine();
-					while ( line.trim().length() > 0 )
-					{
-						int p = line.indexOf( ':' );
-						header.put( line.substring(0,p).trim().toLowerCase(), line.substring(p+1).trim());
-						line = in.readLine();
-					}
+					try { size = Integer.parseInt(contentLength); }
+					catch (NumberFormatException ex) {}
 				}
+
+				// We are looking for the byte separating header from body.
+				// It must be the last byte of the first two sequential new lines.
+				int splitbyte = 0;
+				boolean sbfound = false;
+				while (splitbyte < rlen)
+				{
+					if (buf[splitbyte] == '\r' && buf[++splitbyte] == '\n' && buf[++splitbyte] == '\r' && buf[++splitbyte] == '\n') {
+						sbfound = true;
+						break;
+					}
+					splitbyte++;
+				}
+				splitbyte++;
+
+				// Write the part of body already read to ByteArrayOutputStream f
+				ByteArrayOutputStream f = new ByteArrayOutputStream();
+				f.write(buf, splitbyte, rlen-splitbyte);
+
+				// While Firefox sends on the first read all the data fitting our buffer, chrome and opera sends only the headers even if there is data for the body. So we do some magic here to find out whether we have already consumed part of body, if we have reached the end of the data to be sent or  we should expect the first byte of the body at the next read.
+				if (splitbyte < rlen)
+					size -= rlen - splitbyte +1;
+				else if (!sbfound || size == 0x7FFFFFFFFFFFFFFFl)
+					size = 0;
+
+				// Now read all the body and write it to f
+				buf = new byte[512];
+				while ( rlen >= 0 && size > 0 )
+				{
+					rlen = is.read(buf, 0, 512);
+					size -= rlen;
+					f.write(buf, 0, rlen);
+				}
+
+				// Get the raw body as a byte []
+				byte [] fbuf = f.toByteArray();
+
+				// Create a BufferedReader for easily reading it as string.
+				ByteArrayInputStream bin = new ByteArrayInputStream(fbuf);
+				BufferedReader in = new BufferedReader( new InputStreamReader(bin));
 
 				// If the method is POST, there may be parameters
 				// in data section, too, read it:
 				if ( method.equalsIgnoreCase( "POST" ))
 				{
-					long size = 0x7FFFFFFFFFFFFFFFl;
-					String contentLength = header.getProperty("content-length");
-					if (contentLength != null)
-					{
-						try { size = Integer.parseInt(contentLength); }
-						catch (NumberFormatException ex) {}
+					String contentType = "";
+					String contentTypeHeader = header.getProperty("content-type");
+					StringTokenizer st = new StringTokenizer( contentTypeHeader , "; " );
+					if ( st.hasMoreTokens()) {
+						contentType = st.nextToken();
 					}
-					String postLine = "";
-					char buf[] = new char[512];
-					int read = in.read(buf);
-					while ( read >= 0 && size > 0 && !postLine.endsWith("\r\n") )
+
+					if (contentType.equalsIgnoreCase("multipart/form-data"))
 					{
-						size -= read;
-						postLine += String.valueOf(buf, 0, read);
-						if ( size > 0 )
-							read = in.read(buf);
+						// Handle multipart/form-data
+						if ( !st.hasMoreTokens())
+							sendError( HTTP_BADREQUEST, "BAD REQUEST: Content type is multipart/form-data but boundary missing. Usage: GET /example/file.html" );
+						String boundaryExp = st.nextToken();
+						st = new StringTokenizer( boundaryExp , "=" );
+						if (st.countTokens() != 2)
+							sendError( HTTP_BADREQUEST, "BAD REQUEST: Content type is multipart/form-data but boundary syntax error. Usage: GET /example/file.html" );
+						st.nextToken();
+						String boundary = st.nextToken();
+
+						decodeMultipartData(boundary, fbuf, in, parms, files);
 					}
-					postLine = postLine.trim();
-					decodeParms( postLine, parms );
+					else
+					{
+						// Handle application/x-www-form-urlencoded
+						String postLine = "";
+						char pbuf[] = new char[512];
+						int read = in.read(pbuf);
+						while ( read >= 0 && !postLine.endsWith("\r\n") )
+						{
+							postLine += String.valueOf(pbuf, 0, read);
+							if ( read > 0 )
+								read = in.read(pbuf);
+						}
+						postLine = postLine.trim();
+						decodeParms( postLine, parms );
+
+						System.out.println(postLine);
+						parms.list(System.err);
+					}
 				}
 
 				// Ok, now do the serve()
-				Response r = serve( uri, method, header, parms );
+				Response r = serve( uri, method, header, parms, files );
 				if ( r == null )
 					sendError( HTTP_INTERNALERROR, "SERVER INTERNAL ERROR: Serve() returned a null response." );
 				else
@@ -381,6 +450,244 @@ public class NanoHTTPD
 		}
 
 		/**
+		 * Decodes the sent headers and loads the data into
+		 * java Properties' key - value pairs
+		**/
+		private  void decodeHeader(BufferedReader in, Properties pre, Properties parms, Properties header)
+		{
+			try {
+				// Read the request line
+				String inLine = in.readLine();
+				if (inLine == null) return;
+				StringTokenizer st = new StringTokenizer( inLine );
+				if ( !st.hasMoreTokens())
+					sendError( HTTP_BADREQUEST, "BAD REQUEST: Syntax error. Usage: GET /example/file.html" );
+
+				String method = st.nextToken();
+				pre.put("method", method);
+
+				if ( !st.hasMoreTokens())
+					sendError( HTTP_BADREQUEST, "BAD REQUEST: Missing URI. Usage: GET /example/file.html" );
+
+				String uri = st.nextToken();
+
+				// Decode parameters from the URI
+				int qmi = uri.indexOf( '?' );
+				if ( qmi >= 0 )
+				{
+					decodeParms( uri.substring( qmi+1 ), parms );
+					uri = decodePercent( uri.substring( 0, qmi ));
+				}
+				else uri = decodePercent(uri);
+
+				// If there's another token, it's protocol version,
+				// followed by HTTP headers. Ignore version but parse headers.
+				// NOTE: this now forces header names uppercase since they are
+				// case insensitive and vary by client.
+				if ( st.hasMoreTokens())
+				{
+					String line = in.readLine();
+					while ( line != null && line.trim().length() > 0 )
+					{
+						int p = line.indexOf( ':' );
+						if ( p >= 0 )
+							header.put( line.substring(0,p).trim().toLowerCase(), line.substring(p+1).trim());
+						line = in.readLine();
+					}
+				}
+
+				pre.put("uri", uri);
+			}
+			catch ( IOException ioe )
+			{
+				try
+				{
+					sendError( HTTP_INTERNALERROR, "SERVER INTERNAL ERROR: IOException: " + ioe.getMessage());
+				}
+				catch ( Throwable t ) {}
+			}
+			catch ( InterruptedException ie )
+			{
+				// Thrown by sendError, ignore and exit the thread.
+			}
+		}
+
+		/**
+		 * Decodes the Multipart Body data and put it
+		 * into java Properties' key - value pairs.
+		**/
+		private void decodeMultipartData(String boundary, byte[] fbuf, BufferedReader in, Properties parms, Properties files)
+		{
+			try
+			{
+				int[] bpositions = getBoundaryPositions(fbuf,boundary.getBytes());
+				int boundarycount = 1;
+				String mpline = in.readLine();
+				while ( mpline != null )
+				{
+					if (mpline.indexOf(boundary) == -1)
+						sendError( HTTP_BADREQUEST, "BAD REQUEST: Content type is multipart/form-data but next chunk does not start with boundary. Usage: GET /example/file.html" );
+					boundarycount++;
+					Properties item = new Properties();
+					mpline = in.readLine();
+					while (mpline != null && mpline.trim().length() > 0)
+					{
+						int p = mpline.indexOf( ':' );
+						if (p != -1)
+							item.put( mpline.substring(0,p).trim().toLowerCase(), mpline.substring(p+1).trim());
+						mpline = in.readLine();
+	                }
+	                if (mpline != null)
+					{
+						String contentDisposition = item.getProperty("content-disposition");
+						if (contentDisposition == null)
+						{
+							sendError( HTTP_BADREQUEST, "BAD REQUEST: Content type is multipart/form-data but no content-disposition info found. Usage: GET /example/file.html" );
+						}
+						StringTokenizer st = new StringTokenizer( contentDisposition , "; " );
+						Properties disposition = new Properties();
+						while ( st.hasMoreTokens())
+						{
+							String token = st.nextToken();
+							int p = token.indexOf( '=' );
+							if (p!=-1)
+								disposition.put( token.substring(0,p).trim().toLowerCase(), token.substring(p+1).trim());
+                        }
+						String pname = disposition.getProperty("name");
+						pname = pname.substring(1,pname.length()-1);
+
+						String value = "";
+						if (item.getProperty("content-type") == null) {
+							while (mpline != null && mpline.indexOf(boundary) == -1)
+							{
+								mpline = in.readLine();
+								if ( mpline != null)
+								{
+									int d = mpline.indexOf(boundary);
+									if (d == -1)
+									    value+=mpline;
+									else
+										value+=mpline.substring(0,d-2);
+								}
+							}
+						}
+                        else
+						{
+							if (boundarycount> bpositions.length)
+								sendError( HTTP_INTERNALERROR, "Error processing request" );
+							int offset = stripMultipartHeaders(fbuf, bpositions[boundarycount-2]);
+							String path = saveTmpFile(fbuf, offset, bpositions[boundarycount-1]-offset-4);
+							files.put(pname, path);
+							value = disposition.getProperty("filename");
+							value = value.substring(1,value.length()-1);
+                            do {
+								mpline = in.readLine();
+							} while (mpline != null && mpline.indexOf(boundary) == -1);
+						}
+						parms.put(pname, value);
+					}
+				}
+			}
+			catch ( IOException ioe )
+			{
+				try
+				{
+					sendError( HTTP_INTERNALERROR, "SERVER INTERNAL ERROR: IOException: " + ioe.getMessage());
+				}
+				catch ( Throwable t ) {}
+			}
+			catch ( InterruptedException ie )
+			{
+				// Thrown by sendError, ignore and exit the thread.
+			}
+		}
+
+		/**
+		 * Find the byte positions where multipart boundaries start.
+		**/
+		public int[] getBoundaryPositions(byte[] b, byte[] boundary)
+		{
+			int matchcount = 0;
+			int bytecounter = 0;
+			int matchbyte = -1;
+			Vector matchbytes = new Vector();
+			for (int i=0; i<b.length; i++)
+			{
+				if (b[i] == boundary[matchcount])
+				{
+					if (matchcount == 0)
+						matchbyte = i;
+					matchcount++;
+					if (matchcount==boundary.length)
+					{
+						matchbytes.addElement(new Integer(matchbyte));
+						matchcount = 0;
+						matchbyte = -1;
+					}
+				}
+				else
+				{
+					i -= matchcount;
+					matchcount = 0;
+					matchbyte = -1;
+				}
+			}
+			int[] ret = new int[matchbytes.size()];
+			for (int i=0; i < ret.length; i++)
+			{
+				ret[i] = ((Integer)matchbytes.elementAt(i)).intValue();
+			}
+			return ret;
+		}
+
+		/**
+		 * Retrieves the content of a sent file and saves it
+		 * to a temporery file.
+		 * The full path to the saved file is returned.
+		**/
+		String saveTmpFile(byte[] b, int offset, int len)
+		{
+			String path = "";
+			if (len > 0)
+			{
+				byte[] fc = new byte[len];
+				int j = 0;
+				for (int i=offset; j<len && i<b.length; i++)
+				{
+					fc[j] = b[i];
+					j++;
+				}
+				String tmpdir = System.getProperty("java.io.tmpdir");
+				try {
+					File temp = File.createTempFile("NanoHTTPD", "", new File(tmpdir));
+					OutputStream fstream = new FileOutputStream(temp);
+					fstream.write(fc);
+					fstream.close();
+					path = temp.getAbsolutePath();
+				} catch (Exception e) { // Catch exception if any
+					System.err.println("Error: " + e.getMessage());
+				}
+			}
+			return path;
+		}
+
+
+		/**
+		 * It returns the offset separating multipart file headers
+		 * from the file's data.
+		**/
+		int stripMultipartHeaders(byte[] b, int offset)
+		{
+			int i = 0;
+			for (i=offset; i<b.length; i++)
+			{
+				if (b[i] == '\r' && b[++i] == '\n' && b[++i] == '\r' && b[++i] == '\n')
+					break;
+			}
+			return i+1;
+		}
+
+		/**
 		 * Decodes the percent encoding scheme. <br/>
 		 * For example: "an+example%20string" -> "an example string"
 		 */
@@ -391,20 +698,20 @@ public class NanoHTTPD
 				StringBuffer sb = new StringBuffer();
 				for( int i=0; i<str.length(); i++ )
 				{
-				    char c = str.charAt( i );
-				    switch ( c )
+					char c = str.charAt( i );
+					switch ( c )
 					{
-				        case '+':
-				            sb.append( ' ' );
-				            break;
-				        case '%':
-			                sb.append((char)Integer.parseInt( str.substring(i+1,i+3), 16 ));
-				            i += 2;
-				            break;
-				        default:
-				            sb.append( c );
-				            break;
-				    }
+						case '+':
+							sb.append( ' ' );
+							break;
+						case '%':
+							sb.append((char)Integer.parseInt( str.substring(i+1,i+3), 16 ));
+							i += 2;
+							break;
+						default:
+							sb.append( c );
+							break;
+					}
 				}
 				return new String( sb.toString().getBytes());
 			}
@@ -528,7 +835,7 @@ public class NanoHTTPD
 			{
 				newUri += URLEncoder.encode( tok );
 				// For Java 1.4 you'll want to use this instead:
-				// try { newUri += URLEncoder.encode( tok, "UTF-8" ); } catch ( UnsupportedEncodingException uee )
+				// try { newUri += URLEncoder.encode( tok, "UTF-8" ); } catch ( UnsupportedEncodingException uee ) {}
 			}
 		}
 		return newUri;
@@ -536,8 +843,8 @@ public class NanoHTTPD
 
 	private int myTcpPort;
 	private final ServerSocket myServerSocket;
-	private Thread myThread; 
-	
+	private Thread myThread;
+
 	File myFileDir;
 
 	// ==================================================
@@ -636,6 +943,7 @@ public class NanoHTTPD
 					msg += "<br/>";
 					if ( dir ) msg += "</b>";
 				}
+				msg += "</body></html>";
 				return new Response( HTTP_OK, MIME_HTML, msg );
 			}
 			else
@@ -694,6 +1002,8 @@ public class NanoHTTPD
 	static
 	{
 		StringTokenizer st = new StringTokenizer(
+			"css		text/css "+
+			"js			text/javascript "+
 			"htm		text/html "+
 			"html		text/html "+
 			"txt		text/plain "+
@@ -729,6 +1039,7 @@ public class NanoHTTPD
 	 */
 	private static final String LICENCE =
 		"Copyright (C) 2001,2005-2010 by Jarno Elonen <elonen@iki.fi>\n"+
+		"and Copyright (C) 2010 by Konstantinos Togias <info@ktogias.gr>\n"+
 		"\n"+
 		"Redistribution and use in source and binary forms, with or without\n"+
 		"modification, are permitted provided that the following conditions\n"+
