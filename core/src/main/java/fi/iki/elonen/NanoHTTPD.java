1ca771092a5a39cfea9c4cf843ba7f7c90a1dbcc
Paul Hawke <paul.hawke@gmail.com>
2013-04-14 17:24:21 -0500

    Named the threads created by the server to reflect their use - to aid in profiling performance/memory usage.

--- core/src/main/java/fi/iki/elonen/NanoHTTPD.java
+++ core/src/main/java/fi/iki/elonen/NanoHTTPD.java
@@ -124,6 +124,7 @@ public abstract class NanoHTTPD {
             }
         });
         myThread.setDaemon(true);
+        myThread.setName("NanoHttpd Main Listener");
         myThread.start();
     }
 
@@ -885,10 +886,13 @@ public abstract class NanoHTTPD {
     }
 
     private class DefaultAsyncRunner implements AsyncRunner {
+        private long requestCount;
         @Override
         public void exec(Runnable code) {
+            ++requestCount;
             Thread t = new Thread(code);
             t.setDaemon(true);
+            t.setName("NanoHttpd Request Processor (#" + requestCount + ")");
             t.start();
         }
     }
