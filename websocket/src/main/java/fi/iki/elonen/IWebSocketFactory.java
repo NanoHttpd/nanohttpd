3abbccc225665390f875c10caaa63b4fbe4e8ad1
bruno <bruno.eberhard@pop.ch>
2015-04-04 15:46:35 +0200

    All files merged in one single class. Kind like the NanoHTTPD.

--- websocket/src/main/java/fi/iki/elonen/IWebSocketFactory.java
+++
@@ -1,7 +0,0 @@
-package fi.iki.elonen;
-
-import fi.iki.elonen.NanoHTTPD.IHTTPSession;
-
-public interface IWebSocketFactory {
-    WebSocket openWebSocket(IHTTPSession handshake);
-}
--- websocket/src/main/java/fi/iki/elonen/samples/echo/EchoSocketSample.java
+++ websocket/src/main/java/fi/iki/elonen/samples/echo/EchoSocketSample.java
@@ -1,13 +1,13 @@
 package fi.iki.elonen.samples.echo;
 
-import fi.iki.elonen.NanoWebSocketServer;
-
 import java.io.IOException;
+
+import fi.iki.elonen.NanoWebSocketServer;
 
 public class EchoSocketSample {
     public static void main(String[] args) throws IOException {
         final boolean debugMode = args.length >= 2 && args[1].toLowerCase().equals("-d");
-        NanoWebSocketServer ws = new DebugWebSocketServer(Integer.parseInt(args[0]), debugMode);
+        NanoWebSocketServer ws = new DebugWebSocketServer(args.length > 0 ? Integer.parseInt(args[0]) : 9090, debugMode);
         ws.start();
         System.out.println("Server started, hit Enter to stop.\n");
         try {
